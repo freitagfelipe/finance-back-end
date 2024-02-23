9fe8bad928b8818de4c78bbfcddf356724b92889
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2024-02-23 03:54:49 -0300

    Update main with database setup and router setup

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,26 @@
 use std::{env, error::Error};
 
-use axum::{
-    extract::{Path, Query},
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
-use serde::Deserialize;
+use axum::Router;
+use finance_back_end::{self, connection::Database};
 use tokio::net::TcpListener;
-
-#[derive(Debug, Deserialize)]
-struct HelloParams {
-    name: Option<String>,
-}
-
-async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
-    let name = params.name.as_deref().unwrap_or("person");
-
-    format!("Hello, {name}!")
-}
-
-async fn handler_greeting(Path(name): Path<String>) -> impl IntoResponse {
-    format!("Welcome {name}!")
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
 
-    let listener = TcpListener::bind(
-        env::var("URL").expect("Could not find URL enviroment var")
-    ).await?;
+    let listener =
+        TcpListener::bind(env::var("URL").expect("Could not find URL enviroment var")).await?;
+
+    let pool = sqlx::mysql::MySqlPool::connect(
+        &env::var("DB_URI").expect("Could not find DB_URI enviroment var"),
+    )
+    .await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
 
     let router = Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
-        .route("/hello", get(handler_hello))
-        .route("/greeting/:name", get(handler_greeting));
+        .with_state(Database { pool })
+        .merge(finance_back_end::app_router());
 
     axum::serve(listener, router.into_make_service()).await?;
 
