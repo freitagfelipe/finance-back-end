5442fd51cdf62eba3713f11fe11c738e877da379
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2024-02-23 04:53:49 -0300

    Update controller trait to return a router with app state

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -3,11 +3,13 @@ use axum::{
     Router,
 };
 
+use crate::AppState;
+
 trait Controller {
-    fn get_controller_router() -> Router;
+    fn get_controller_router() -> Router<AppState>;
 }
 
-pub fn app_router() -> Router {
+pub fn app_router() -> Router<AppState> {
     Router::new()
         .fallback(|uri: Uri| async move { (StatusCode::NOT_FOUND, format!("No route for {uri}")) })
 }
