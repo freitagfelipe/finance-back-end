39207da8df5deecf413a689bd440ea7fb82634e2
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2024-02-23 06:39:50 -0300

    Register user controller routes in app routes

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,3 +1,5 @@
+pub mod user;
+
 use axum::{
     http::{StatusCode, Uri},
     Router,
@@ -5,11 +7,14 @@ use axum::{
 
 use crate::AppState;
 
+use self::user::UserController;
+
 trait Controller {
     fn get_controller_router() -> Router<AppState>;
 }
 
 pub fn app_router() -> Router<AppState> {
     Router::new()
+        .merge(UserController::get_controller_router())
         .fallback(|uri: Uri| async move { (StatusCode::NOT_FOUND, format!("No route for {uri}")) })
 }
