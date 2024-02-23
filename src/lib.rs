5c937aecfda94e10eb4db25b88d3692c0cbd1920
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2024-02-23 04:54:14 -0300

    Implement a struct to represent the app state

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,10 @@ pub mod connection;
 mod controller;
 mod model;
 
+use connection::Database;
 pub use controller::app_router;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub database: Database,
+}
